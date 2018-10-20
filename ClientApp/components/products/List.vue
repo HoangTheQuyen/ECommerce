37a4501a24edda0709b02d6d0c69740da7b28f52
HoangTheQuyen <38252060+HoangTheQuyen@users.noreply.github.com>
2018-10-20 16:25:14 +0700

    b

---
+++ ClientApp/components/products/List.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="products">
+    <h1>Products</h1>
+    <div class="list">
+        <div class="item" v-for="product in products" :key="product.slug">
+            <h3 @click="select(product)">{{ product.name }}</h3>
+
+            <img :src="product.thumbnail" :alt="product.name" @click="select(product)" />
+
+            <p @click="select(product)">{{ product.shortDescription }} </p>
+
+            <p @click="select(product)">{{ product.price }}</p>
+            
+            <hr />
+        </div>
+    </div>
+</div>
+</template>
